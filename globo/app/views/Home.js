7e0f0ab09d8435bcdb33043af8d966966ed8c953
Arnon Ruangthanawes <arnon.raungthanawes@unico.com.au>
2018-10-31 12:37:39 +1100

    Implement initial screen

---
+++ globo/app/views/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Header } from '../sections/Header';
+import { Hero } from '../sections/Hero';
+import { Menu } from '../sections/Menu';
+
+export class Home extends React.Component {
+   render() {
+     return (
+       <View style={styles.container}>
+        <Header message = 'Press to Login' />
+        <Hero />
+        <Menu />
+       </View>
+     );
+   }
+ }
+
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     flexDirection: 'column'
+   },
+ });
