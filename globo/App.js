f169745a07ce5e5b5e3a16306a20653b3e475a7b
Arnon Ruangthanawes <arnon.raungthanawes@unico.com.au>
2018-11-01 12:00:35 +1100

    Add video detail page

--- globo/App.js
+++ globo/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Home } from './app/views/Home';
 import { Contact } from './app/views/Contact';
 import { Video } from './app/views/Video';
+import { VideoDetail } from './app/views/VideoDetail';
 import { createStackNavigator } from 'react-navigation';
 
 const MyRoutes = createStackNavigator({
@@ -14,6 +15,9 @@ const MyRoutes = createStackNavigator({
   LessonsRT: {
     screen: Video
   },
+  VideoDetailRT: {
+    screen: VideoDetail
+  }
 },
   {
     initialRouteName: 'HomeRT'
--- globo/app/views/Video.js
+++ globo/app/views/Video.js
@@ -16,43 +16,46 @@ export class Video extends React.Component {
     return fetch(
       'https://www.googleapis.com/youtube/v3/search?part=snippet&q=pluralsight&key=AIzaSyDTzRqxq4Mm0cInB9CFAGtrJjS0OuGnKAU'
     )
-    .then((response) => response.json())
-    .then((responseJson) => {
-      if (responseJson.items) {
-        this.setState({
-          listLoaded: true,
-          videoList: Array.from(responseJson.items)
-        });
-      }
-      else {
+      .then((response) => response.json())
+      .then((responseJson) => {
+        if (responseJson.items) {
+          this.setState({
+            listLoaded: true,
+            videoList: Array.from(responseJson.items)
+          });
+        }
+        else {
           console.error(responseJson);
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   render() {
+    const { navigate } = this.props.navigation;
+
     return (
       <View>
-        { this.state.listLoaded && (
+        {this.state.listLoaded && (
           <View style={{ paddingTop: 30 }}>
             <FlatList
-              data={ this.state.videoList }
-              renderItem={ ({item}) =>
+              data={this.state.videoList}
+              renderItem={({ item }) =>
                 <TubeItem
+                  navigate={navigate}
                   id={item.id.videoId}
                   title={item.snippet.title}
                   imageSrc={item.snippet.thumbnails.high.url}
                 />
               }
               keyExtractor={(item, index) => index.toString()}
-          />
+            />
           </View>
         )}
 
-        { !this.state.listLoaded && (
+        {!this.state.listLoaded && (
           <View style={{ paddingTop: 30 }}>
             <Text> LOADING </Text>
           </View>
@@ -66,21 +69,23 @@ export class Video extends React.Component {
 export class TubeItem extends React.Component {
 
   onPress = () => {
-    console.log(this.props.id);
+    if (this.props.id) {
+      this.props.navigate('VideoDetailRT', {ytubeId: this.props.id});
+    }
   }
 
   render() {
     return (
       <TouchableWithoutFeedback onPress={this.onPress}>
-      <View style={{ paddingTop: 20, alignItems: 'center' }}>
-        <Image
-          style={{ width: '100%', height: 200 }}
-          source={{ uri: this.props.imageSrc }}
-        />
-        <Text>
-          {this.props.title}
-        </Text>
-      </View>
+        <View style={{ paddingTop: 20, alignItems: 'center' }}>
+          <Image
+            style={{ width: '100%', height: 200 }}
+            source={{ uri: this.props.imageSrc }}
+          />
+          <Text>
+            {this.props.title}
+          </Text>
+        </View>
       </TouchableWithoutFeedback>
     );
   }
